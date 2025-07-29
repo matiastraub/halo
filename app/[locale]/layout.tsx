898fed94c2629da2aca170c9df8dc5555709a42e
Matias Traub <matias.traub@gmail.com>
2025-07-28 20:21:02 -0400

    Fix translation issues

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,6 +7,8 @@ import { getMessages } from 'next-intl/server'
 import { NextIntlClientProvider } from 'next-intl'
 import NavBar from './layout/NavBar'
 import Footer from './layout/Footer'
+import { notFound } from 'next/navigation'
+import { routing } from 'i18n/routing'
 
 type Props = Readonly<{
   children: React.ReactNode
@@ -29,29 +31,33 @@ export const metadata: Metadata = {
     'Impulsa tu negocio con agentes de IA, automatización inteligente, capacitación en TI y soluciones tecnológicas a tu medida.'
 }
 
-export default async function RootLayout(props: Props) {
+export default async function RootLayout({ children, params }: Props) {
   const GA_ID = process.env.NEXT_PUBLIC_GA_ID
 
-  const locale = props.params.locale
+  const { locale } = await params
 
   const messages = await getMessages({ locale })
-  if (locale)
-    return (
-      <html lang={locale} className="h-full bg-white">
-        <head>
-          <Script
-            src="https://kit.fontawesome.com/8e98006f77.js"
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
-          />
-          {GA_ID && (
-            <>
-              <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-                strategy="afterInteractive"
-              />
-              <Script id="ga-init" strategy="afterInteractive">
-                {`
+
+  type Locale = 'es' | 'en'
+  if (!routing.locales.includes(locale as Locale)) {
+    notFound()
+  }
+  return (
+    <html lang={locale} className="h-full bg-white">
+      <head>
+        <Script
+          src="https://kit.fontawesome.com/8e98006f77.js"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+        {GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="ga-init" strategy="afterInteractive">
+              {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
@@ -59,29 +65,29 @@ export default async function RootLayout(props: Props) {
                   page_path: window.location.pathname,
                 });
               `}
-              </Script>
-            </>
-          )}
-        </head>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <NextIntlClientProvider messages={messages} locale={locale}>
-            <div className="frame-root">
-              <div className="frame-content">
-                <div className="[font-family:var(--font-family-body)]">
-                  <div>
-                    <header className="relative z-50 code-section" id="global-header">
-                      <NavBar />
-                    </header>
-                  </div>
-                  {props.children}
-                  <div>
-                    <Footer />
-                  </div>
+            </Script>
+          </>
+        )}
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NextIntlClientProvider messages={messages} locale={locale}>
+          <div className="frame-root">
+            <div className="frame-content">
+              <div className="[font-family:var(--font-family-body)]">
+                <div>
+                  <header className="relative z-50 code-section" id="global-header">
+                    <NavBar />
+                  </header>
+                </div>
+                {children}
+                <div>
+                  <Footer />
                 </div>
               </div>
             </div>
-          </NextIntlClientProvider>
-        </body>
-      </html>
-    )
+          </div>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  )
 }
